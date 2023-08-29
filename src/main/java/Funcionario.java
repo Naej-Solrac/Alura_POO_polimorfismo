36e4256e6379c910d79564b19aa57186fc060ac5
Jean Gonzales <expretapador3@gmail.com>
2023-08-28 21:53:31 -0500

    corrigiento

--- src/main/java/Funcionario.java
+++ src/main/java/Funcionario.java
@@ -29,7 +29,7 @@ public class Funcionario {
 
     public double getBonificacion () {
         // si tipo = 1 es gerente
-        // si tipo = 0 es gerente
+        // si tipo = 0 es funcionario
         if (this.tipo == 0){
         return this.salario * 0.1;
         } else if (this.tipo == 1) {
