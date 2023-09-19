958c114fafb9512c864b8e0083e2415be0c05e8a
Jean Gonzales <expretapador3@gmail.com>
2023-09-19 15:55:49 -0500

    Creo clase controlBonificacion

---
+++ src/main/java/ControlBonificacion.java
@@ -0,0 +1,7 @@
+public class ControlBonificacion {
+    private double suma;
+
+    public double registrarSalario(Funcionario funcionario) {
+        return this.suma = funcionario.getBonificacion() + this.suma;
+    }
+}
